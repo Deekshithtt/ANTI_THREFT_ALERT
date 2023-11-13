eb4314c5f04d5f9c455fd71f47d235cb1fa32149
Deekshithtt <142730500+Deekshithtt@users.noreply.github.com>
2023-11-13 10:36:28 +0530

    Add files via upload

---
+++ Anti_threft/Anti_threft.ino
@@ -0,0 +1,45 @@
+#define BLYNK_TEMPLATE_ID "TMPL35qT6ItIF"
+#define BLYNK_TEMPLATE_NAME "Theft Alert Notification"
+#define BLYNK_AUTH_TOKEN "JwYZl3qpPd3r0Z0DQWz9p0otyWOLKn4o"
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+ 
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "Beast07";  // type your wifi name
+char pass[] = "78847884";  // type your wifi password
+ 
+#define PIR_SENSOR  4
+BlynkTimer timer;
+//int flag=0;
+void notifyOnTheft()
+{
+  int isTheftAlert = digitalRead(PIR_SENSOR);
+  Serial.println(isTheftAlert);
+  if (isTheftAlert==1) {
+    Serial.println("Theft Alert in Home");
+    // Blynk.email("shameer50@gmail.com", "Alert", "Theft Alert in Home");
+    //Blynk.notify("Alert : Theft Alert in Home");
+    Blynk.logEvent("theft_s","Theft Alert in Home");
+//    flag=1;
+  }
+  else if (isTheftAlert==0)
+  {
+   // flag=0;
+  }
+}
+
+void setup(){
+  pinMode(PIR_SENSOR, INPUT);
+   Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  //dht.begin();
+  timer.setInterval(5000L, notifyOnTheft);
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
